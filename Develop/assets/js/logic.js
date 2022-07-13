ab608166858e096e249845c23e39d31de2cc5e14
Andrew Nguyen-Tran <ann013@ucsd.edu>
2022-07-12 19:05:36 -0700

    challange 4 hw

---
+++ Develop/assets/js/logic.js
@@ -0,0 +1,54 @@
+// TODO: add game logic
+
+///Timer for the game ////////
+
+const startMinutes = 10;
+let time = startMinutes*60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updatecountdown,1000);
+
+function updatecountdown() {
+const minutes = Math.floor(time/60);
+let seconds = time % 60;
+
+countdownEl.innerHTML = `${minutes}:${seconds}`; 
+time--
+}
+
+
+///Start feature for the game ////
+
+const startButton = document.getElementById('start-btn')
+const questionBox = document.getElementById('Question')
+const quizIntroduction = document.getElementById('introduction')
+
+let shuffleQuestions, currentQuestionIndex 
+const questionElement = document.getElementById('questionHeader')
+const answerElementButtons = document.getElementById('answer-btn')
+
+startButton.addEventListener('click',startGame)
+
+function startGame(){
+console.log('start')
+shuffleQuestions = questionA.sort(() => Math.random()-.5)
+currentQuestionIndex = 0 
+quizIntroduction.style.display ='none'
+questionBox.style.display = 'block'
+nextQuestion()
+
+
+}
+
+function nextQuestion(){
+
+    showQuestion(shuffleQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(questionA) {
+questionElement.innertext = questionA.question1
+
+
+}
---
+++ Develop/assets/js/questions.js
@@ -0,0 +1,52 @@
+// TODO: store all questions here
+// NOTE: hint - can use an array that contains objects
+
+
+const questionA = [
+
+    {
+      question1: 'What does HTML stand for?',
+      answers:[
+      {text:'HyperText Markup Language', correct:true},
+      {text:'HyperText Making Language', correct:false},
+      {text:'High Markup Language', correct:false},
+      {text:'HyperText Markup Ladder', correct:false}
+      ]
+
+    },
+
+    {
+        question: 'What does CSS stand for?',
+        answers:[
+        {text:'Cascading Style Sheets', correct:true},
+        {text:'Center Style Sheets', correct:false},
+        {text:'Center Super Sheets', correct:false},
+        {text:'Cascading Super Sheets', correct:false}
+        ]
+  
+      },
+
+
+      {
+        question: 'What percentage of the web uses Javascript',
+        answers:[
+        {text:'98%', correct:true},
+        {text:'86%', correct:false},
+        {text:'83%', correct:false},
+        {text:'5%', correct:false}
+        ]
+  
+      },
+
+      {
+        question: 'What would you use to style HTML?',
+        answers:[
+        {text:'CSS', correct:true},
+        {text:'HTML', correct:false},
+        {text:'Javascript', correct:false},
+        {text:'Java', correct:false}
+        ]
+  
+      }
+
+]
